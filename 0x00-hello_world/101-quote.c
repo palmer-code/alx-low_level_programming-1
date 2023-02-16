a2d8ebcbec9c9b49906af505a6713de79c855345
Mugambi12 <silasmungiria16@gmail.com>
2023-02-16 14:38:27 -0800

    Task 5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,21 +2,21 @@
 
 /**
  * main - Entry point
- * Description: This function uses inline assembly to print a string to the console using the syscall function in x86_64 architecture.
+ * Description: Uses inline assembly to print a string using syscall function in x86_64 architecture.
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-        char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        long l = 59;
-        long fd = 1;
-        long syscall = 1;
-        long ret = 0;
-        __asm__ ("syscall"
-                : "=a" (ret)
-                : "a" (syscall),
-                  "D" (fd),
-                  "S" (s),
-                  "d" (l));
-        return (1);
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	long l = 59;
+	long fd = 1;
+	long syscall = 1;
+	long ret = 0;
+	__asm__ ("syscall"
+		: "=a" (ret)
+		: "a" (syscall),
+		  "D" (fd),
+		  "S" (s),
+		  "d" (1));
+		  (return (1);
 }

3322899317f33edb445600dd07f7ce369fdb2b87
Mugambi12 <silasmungiria16@gmail.com>
2023-02-21 14:44:13 -0800

    Task 0 on putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * Description: This program prints 'Holberton \n' to the console
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "Holberton\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}

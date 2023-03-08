a2ad072324f4e53c7884e928542c89484ac3e0cf
Mugambi12 <silasmungiria16@gmail.com>
2023-03-08 04:38:06 -0800

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,21 +12,24 @@
  * @str: The string to be measured.
  *
  * Return: The length.
+ * 
+ * Author: Silas Mugambi
+ * Date: 2023-03-08
  */
 int strlen_no_wilds(char *str)
 {
-    int len = 0, index = 0;
+	int len = 0, index = 0;
 
-    if (*(str + index))
-    {
-        if (*str != '*')
-            len++;
+	if (*(str + index))
+	{
+		if (*str != '*')
+			len++;
 
-        index++;
-        len += strlen_no_wilds(str + index);
-    }
+		index++;
+		len += strlen_no_wilds(str + index);
+	}
 
-    return (len);
+	return (len);
 }
 
 /**
@@ -37,11 +40,11 @@ int strlen_no_wilds(char *str)
  */
 void iterate_wild(char **wildstr)
 {
-    if (**wildstr == '*')
-    {
-        (*wildstr)++;
-        iterate_wild(wildstr);
-    }
+	if (**wildstr == '*')
+	{
+		(*wildstr)++;
+		iterate_wild(wildstr);
+	}
 }
 
 /**
@@ -57,19 +60,19 @@ void iterate_wild(char **wildstr)
  */
 char *postfix_match(char *str, char *postfix)
 {
-    int str_len = strlen_no_wilds(str) - 1;
-    int postfix_len = strlen_no_wilds(postfix) - 1;
+	int str_len = strlen_no_wilds(str) - 1;
+	int postfix_len = strlen_no_wilds(postfix) - 1;
 
-    if (*postfix == '*')
-        iterate_wild(&postfix);
+	if (*postfix == '*')
+		iterate_wild(&postfix);
 
-    if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
-    {
-        postfix++;
-        return (postfix_match(str, postfix));
-    }
+	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
+	{
+		postfix++;
+		return (postfix_match(str, postfix));
+	}
 
-    return (postfix);
+	return (postfix);
 }
 
 /**
@@ -83,17 +86,17 @@ char *postfix_match(char *str, char *postfix)
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        iterate_wild(&s2);
-        s2 = postfix_match(s1, s2);
-    }
+	if (*s2 == '*')
+	{
+		iterate_wild(&s2);
+		s2 = postfix_match(s1, s2);
+	}
 
-    if (*s2 == '\0')
-        return (1);
+	if (*s2 == '\0')
+		return (1);
 
-    if (*s1 != *s2)
-        return (0);
+	if (*s1 != *s2)
+		return (0);
 
-    return (wildcmp(++s1, ++s2));
+	return (wildcmp(++s1, ++s2));
 }

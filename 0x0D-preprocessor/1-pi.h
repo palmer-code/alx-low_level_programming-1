06a917a24ae30f8247baa7cbbcd55d23940fb599
mugambi12 <silasmungiria16@gmail.com>
2023-03-20 17:02:53 +0300

    header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Desc: This header file defines a macro named PI, which is an abbreviation
+ *       for the mathematical constant 3.14159265359.
+ *
+ * Author: Silas Mugambi
+ */
+
+#define PI 3.14159265359
+
+#endif

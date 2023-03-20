4dc39cd1f95219c6a991953b5e7dc1dadf06f919
mugambi12 <silasmungiria16@gmail.com>
2023-03-20 16:57:25 +0300

    header file that defines a macro named SIZE as an abbreviation for the token 1024.

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Desc: This header file defines a macro named SIZE, which is an abbreviation
+ *       for the numeric value 1024.
+ *
+ * Author: Silas Mugambi
+ */
+
+#define SIZE 1024
+
+#endif
